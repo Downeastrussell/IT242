d53d505fc3f11841787993e8773979118061f992
Russell Miller <45610992+Downeastrussell@users.noreply.github.com>
2020-02-17 15:04:05 -0500

    Making one repo for whole class
    IT 242 repo consolidation

---
+++ chapter2/fundamentals/fundamentals/fundamentals/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DateTime is the type of the purchaseData variable.
+            DateTime purchaseDate = DateTime.Now;
+
+            /*
+                string is the type of the lastName variable. It
+                tells the compiler that the lastName variable can
+                ONLY hold a string value.
+            */
+            string lastName = "Miller";
+
+            /*
+                C# now supports implicitly typing of a variable. The
+                type of the variable will be determined, by the
+                compiler, at compile time.
+            */
+            var firstName = "alsdkf";
+            var favoriteColor = "blue";
+
+
+            /*
+                String interpolation in C# is similar to string interpolation in JavaScript,
+                but there is a different syntax.
+
+                An equivalent statement in JavaScript would be:
+                console.log(`${firstName} ${lastName} purchased on ${purchaseDate}`);
+            */
+            Console.WriteLine($"Russell {lastName} purchased on {purchaseDate}");
+            Console.WriteLine($"{firstName}'s favorite color is {favoriteColor}");
+        }
+    }
+}
