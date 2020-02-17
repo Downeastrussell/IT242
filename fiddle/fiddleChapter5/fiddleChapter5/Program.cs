cf489d38c03944213fe647981cc996f63e1f29a1
Russell Miller <45610992+Downeastrussell@users.noreply.github.com>
2020-02-17 14:49:51 -0500

    moved into correct repa
    github DeskTop is terrible

---
+++ fiddle/fiddleChapter5/fiddleChapter5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fiddleChapter5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary's are collections of a key/value pairs - just strongly typed. They can be of any type(int, string, bool, custom, etc).
+
+            //////////////////////////////////////
+            ////How to make a dictionary in C#///
+            ////////////////////////////////////
+            //makes a dictionary named 'toysSold' and adds key value pairs at the sane time, you can also make an empty dictionary and use nameOfDictionary.Add(Key, Value)
+            Dictionary<int, int> numbers = new Dictionary<int, int>() {
+                    {3, 344},
+                    {4, 763},
+                    {5, 551},
+                    {30, 200}
+                };
+
+            /////////////////////////////////////////////////////////////////////////////////////////
+            ////how to use .Add(Key, Value) to add KeyValuePairs to the dictionary named "toysSold"///
+            //////////////////////////////////////////////////////////////////////////////////////////
+            //  "GI Joe" = Key   and   430 = Value || together they make a KeyValuePair
+            numbers.Add(31, 430);
+            //access and print this newly added Value(430) to the console by using the key of "GI Joe"
+            Console.WriteLine(numbers[30]);
+
+        }
+    }
+}
---
+++ fiddle/fiddleChapter6/chapter6Lecture/chapter6Lecture/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace chapter6Lecture
+{
+    class Program
+    {
+
+        public class Writer
+        {
+            public Writer(string prefix)
+            {
+                Prefix = prefix;
+            }
+
+            public string Prefix { get; set; }
+
+            public void Write(string message)
+            {
+                Console.WriteLine(Prefix + message);
+            }
+        }
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+
+            // The output variable's type is `string` -- a built in type
+            string output = "Nashville Software School";
+
+            // The author variable's type is Writer -- a custom type you defined
+            Writer author = new Writer("I do declare: ");
+            author.Write(output);
+
+            author.Prefix = "That's right, I said: ";
+            author.Write(output);
+
+
+        }
+    }
+}
